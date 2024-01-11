f6a7c700fd6c2d160c17ca169756ee3fd51a8129
Eddie Jimenez <eddiejimenez@Eddies-MacBook-Pro.local>
2024-01-11 00:26:25 -0800

    refactoring, separating jsons

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,6 @@ import {
   tier3,
   tier4,
 } from "data";
-// import { gasItem, item1, item2, item3, item4, rootItem } from "data";
 
 type Item = {
   method?: string;
@@ -22,67 +21,67 @@ type Items = {
   [key: string]: Item;
 };
 
-// TODO: change to enum
-type MethodsType = {
-  [key: string]: string;
-};
-
-type SomeThing = {
-  sources?: SomeThing[] | undefined;
-  planets?: string[] | undefined;
+type TreeNode = {
   name: string;
   method?: string | undefined;
+  planets?: string[] | undefined;
+  sources?: TreeNode[] | undefined;
 };
+
+const STR = [
+  "graph TD",
+  "classDef rootElement fill:#6f9",
+  "classDef compoundElement fill:#f96",
+  "classDef planettt fill:#6f9",
+];
+
+enum METHODS {
+  "AC" = "[/Atmospheric Condenser\\]",
+  "BP" = "[[Backpack Printer]]",
+  "CL" = "[/Chemistry Lab\\]",
+  "LP" = "[[Large Printer]]",
+  "MP" = "[[Medium Printer]]",
+  "SP" = "[[Small Printer]]",
+  "SF" = "[(Smelting Furnace)]",
+}
 
 const DEFAULT = "RTG";
 
 export const App = (): JSX.Element => {
-  const [element, setElement] = useState<string | undefined>(undefined);
+  console.log("\n\n\n");
+
+  const [resource, setResource] = useState<string>(DEFAULT);
+  const [maxDepth, setMaxDepth] = useState<number>(0);
+  const [tree, setTree] = useState<TreeNode | undefined>(undefined);
   const [hasCondenser, setHasCondenser] = useState<boolean>(false);
   const [hasOtherMachines, setHasOtherMachines] = useState<boolean>(false);
-  const [maxDepth, setMaxDepth] = useState<number>(0);
   const craftFlowRef = useRef<HTMLDivElement | null>(null);
 
   const allCraftedItems = useMemo((): Items => {
     return { ...refined, ...tier1, ...tier2, ...tier3, ...tier4 };
   }, []);
+
   const allItems = useMemo((): Items => {
     return { ...allCraftedItems, ...atmospheric, ...natural };
   }, [allCraftedItems]);
 
-  // TODO: move to const
-  const methods = useMemo((): MethodsType => {
-    return {
-      "Atmospheric Condenser": "[/Atmospheric Condenser\\]",
-      "Backpack Printer": "[[Backpack Printer]]",
-      "Chemistry Lab": "[/Chemistry Lab\\]",
-      "Large Printer": "[[Large Printer]]",
-      "Medium Printer": "[[Medium Printer]]",
-      "Small Printer": "[[Small Printer]]",
-      "Smelting Furnace": "[(Smelting Furnace)]",
-    };
-  }, []);
+  const extendedArrow = useMemo(() => {
+    return hasCondenser && hasOtherMachines ? "--->" : "-->";
+  }, [hasCondenser, hasOtherMachines]);
 
-  const bleh = useCallback(
-    ({ depth, name }: { depth: number; name: string }): SomeThing => {
-      // console.log("bleh", depth);
+  const generateTree = useCallback(
+    ({ name, depth = 0 }: { name: string; depth?: number }): TreeNode => {
       setMaxDepth((m) => Math.max(m, depth));
-      // console.log("asdfwet");
 
       const { method, planets, sources } = allItems[name] ?? {};
 
       if (planets !== undefined) {
-        // if (method === "Atmospheric Condenser") {
-        //   setHasCondenser(true);
-        // }
-        method === "Atmospheric Condenser"
-          ? setHasCondenser(true)
-          : setHasOtherMachines(true);
+        method === "AC" ? setHasCondenser(true) : setHasOtherMachines(true);
       }
 
       const sourceList = sources
         ?.sort()
-        .map((s) => bleh({ depth: depth + 1, name: s }));
+        .map((s) => generateTree({ depth: depth + 1, name: s }));
 
       return {
         name,
@@ -94,98 +93,72 @@ export const App = (): JSX.Element => {
     [allItems],
   );
 
-  console.log("outside maxdepth", maxDepth);
   const buildChart = useCallback(
-    (asdf: SomeThing, id = "0"): string[] => {
-      const { method, planets, sources } = asdf;
-      // console.log("buildchart maxdepth", maxDepth);
+    (asdf: TreeNode, id = "0"): string[] => {
+      const { method, name, planets, sources } = asdf;
       const prevId = id.replace(/-\d+$/, "");
       const depth = (id.match(/-/g) || []).length;
-      // const dash = "-".repeat((maxDepth - depth) * 2);
-      console.log("asdf", maxDepth, depth);
-      // const dash = "--";
-      const str: string[] = [];
-
       const calc = Math.max(maxDepth - depth, 0);
       const dash = maxDepth === 0 ? "--" : "-".repeat(calc * 2);
+      const str: string[] = [];
+
+      const m = METHODS[method as keyof typeof METHODS];
+      const isAtmosphericItem =
+        atmospheric[name as keyof typeof atmospheric] !== undefined;
+      const isNaturalItem = natural[name as keyof typeof natural] !== undefined;
+
+      const p =
+        planets !== undefined ? `planet${id}([${planets.join("<br>")}])` : "";
+
+      if (isAtmosphericItem) {
+        str.push(`${p} --> method${id}`);
+        str.push(`method${id}${m} --> item${id}`);
+      } else if (isNaturalItem) {
+        str.push(`${p} ${extendedArrow} item${id}`);
+      } else {
+        str.push(`method${id}${m} --> item${id}`);
+      }
 
       if (planets !== undefined) {
-        if (method !== undefined) {
-          str.push(`planet${id}([${planets.join("<br>")}]) --> method${id}`);
-          str.push(`method${id}${methods[method]} --> item${id}`);
-        } else {
-          const dash = hasCondenser && hasOtherMachines ? "-" : "";
-          str.push(
-            `planet${id}([${planets.join("<br>")}]) ${dash}--> item${id}`,
-          );
-        }
         str.push(`class planet${id} planettt`);
-
-        str.push(`item${id}[${asdf.name}] ${dash}--> method${prevId}`);
-        str.push(`class item${id} compoundElement`);
-      } else if (sources !== undefined) {
-        const methodStr = id !== "0" ? ` --> method${prevId}` : "";
-        str.push(`item${id}[${asdf.name}] ${methodStr}`);
-        str.push(`class item${id} compoundElement`);
-        str.push(`method${id}${methods[method!]} --> item${id}`);
       }
 
-      if (sources !== undefined) {
-        sources.forEach((source, i) => {
-          const nextId = `${id}-${i}`;
-          str.push(...buildChart(source, nextId));
-        });
-      }
+      const s = ` ${planets === undefined ? "" : dash}--> method${prevId}`;
+
+      const methodStr = id === "0" ? "" : s;
+
+      str.push(`item${id}[${name}]${methodStr}`);
+      str.push(`class item${id} compoundElement`);
+
+      (sources || []).forEach((source, i) => {
+        str.push(...buildChart(source, `${id}-${i}`));
+      });
 
       return str;
     },
-    [hasCondenser, hasOtherMachines, maxDepth, methods],
+    [extendedArrow, maxDepth],
   );
 
-  const processItem2 = useCallback(
-    (name: string): string => {
-      // let maxDepth = 0;
-
-      const asdf = bleh({ depth: 0, name });
-      // console.log("max", maxDepth);
-      console.log("asdf", asdf);
-
-      const str = [
-        "graph TD",
-        "classDef rootElement fill:#6f9",
-        "classDef compoundElement fill:#f96",
-        "classDef planettt fill:#6f9",
-      ];
-
-      return str.concat(buildChart(asdf)).join(";\n");
-    },
-    [bleh, buildChart],
-  );
-
-  const graph = useMemo((): string => {
-    if (element === undefined) {
-      return DEFAULT;
-    }
-
+  const handleChange = useCallback((e: { target: { value: string } }) => {
     setMaxDepth(0);
     setHasCondenser(false);
     setHasOtherMachines(false);
-    const blah = processItem2(element);
-
-    // console.log("processItem2", blah);
-
-    return blah;
-  }, [element, processItem2]);
-
-  const handleChange = useCallback((e: { target: { value: string } }) => {
-    setElement(e.target.value);
+    setTree(undefined);
+    setResource(e.target.value);
   }, []);
 
   useEffect(() => {
-    if (element === undefined) {
+    if (craftFlowRef.current) {
+      mermaid.mermaidAPI.initialize({ startOnLoad: true });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tree === undefined) {
       return;
     }
 
+    const graph = STR.concat(buildChart(tree)).join(";\n");
     mermaid.mermaidAPI
       .render("flow", graph, craftFlowRef.current || undefined)
       .then((result) => {
@@ -196,20 +169,15 @@ export const App = (): JSX.Element => {
       .catch((error) => {
         console.error(error);
       });
-  }, [element, graph]);
+  }, [buildChart, tree]);
 
   useEffect(() => {
-    if (craftFlowRef.current) {
-      mermaid.mermaidAPI.initialize({ startOnLoad: true });
-      setElement(DEFAULT);
-    }
-  }, []);
-
-  console.log("\n\n\n");
+    setTree(generateTree({ name: resource }));
+  }, [resource, generateTree]);
 
   return (
     <>
-      <select onChange={handleChange} value={element}>
+      <select onChange={handleChange} value={resource}>
         {Object.keys(allCraftedItems)
           .sort()
           .map((itemName) => {
